d6185e73d2ad00e255497e53c5905e4692c71ec1
mehdi00x <abouelmouahibmehdi10@example.com>
2023-07-14 18:35:47 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
+
