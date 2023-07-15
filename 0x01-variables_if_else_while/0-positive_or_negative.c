efb872d8ff047f22efb1cc175278d3a5f717db94
mehdi00x <abouelmouahibmehdi10@example.com>
2023-07-14 17:24:22 -0700

    zero_neg_pos

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%dis positive\n", n);
+	else if (n < 0)
+		printf("%dis negative\n", n);
+	else
+		printf("%dis zero\n", n);
+	return (0);
+}
+
